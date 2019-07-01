41cbd43b9aaa7fca38b2c3a759311451140f3955
rayenstaali <52163867+rayenstaali@users.noreply.github.com>
2019-07-01 11:51:14 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+function bold(){
+    document.getElementById('box').style.fontWeight="bold";
+}
+function italic(){
+    document.getElementById('box').style.fontStyle="italic";
+}
+function underline(){
+    document.getElementById('box').style.textDecoration="underline";
+}
+function aman(){
+    document.getElementById('box').style.fontSize=document.getElementById('ks').value 
+}
+function family(){
+    document.getElementById('box').style.fontFamily=document.getElementById('rs').textContent
+}
+ 
